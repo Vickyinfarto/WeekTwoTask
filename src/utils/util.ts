42386f1178e0c49595a92268bf304c39e7c89a27
Vickyinfarto <vickyinfarto@gmail.com>
2025-06-13 00:40:12 +0530

    testscript

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,6 +1,6 @@
 export function validateAndLogPageTitle(actual: string, expected: string) {
   if (actual === expected) {
-    console.log(`[SUCCESS] Page title validated: ${actual}`);
+    console.log(`[SUCCESS] Page title validated as expected: ${actual}`);
   } else {
     console.log(`[FAILED] Page title mismatch: ${actual}`);
   }
